24bdb1af3e11e974a5c5dab6efafa9f463fa88ef
NFERNANDEZ <NFERNANDEZ@15075a66-5c5d-40c6-9c28-692553124c8e>
2018-06-25 12:44:25 +0000

    git-svn-id: https://svn.scae.redsara.es/svn/sim/trunk@46 15075a66-5c5d-40c6-9c28-692553124c8e

--- PlataformaMensajeriaIOP/src/es/minhap/plataformamensajeria/iop/services/recepcionEstadoSMS/IRecepcionEstadoSMSService.java
+++
@@ -1,9 +0,0 @@
-package es.minhap.plataformamensajeria.iop.services.recepcionEstadoSMS;
-
-import es.minhap.plataformamensajeria.iop.beans.RecepcionEstadoSMSXMLBean;
-
-public interface IRecepcionEstadoSMSService {
-	public  RespuestaEstadoSMSXMLBean recibirEstadoSMS (RecepcionEstadoSMSXMLBean recepcionEstadoSMS);
-	
-	public String recibirEstadoSMSXML (RecepcionEstadoSMSXMLBean recepcionEstadoSMS);
-}
