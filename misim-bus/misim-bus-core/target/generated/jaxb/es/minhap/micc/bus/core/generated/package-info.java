99c9e8c9a2ddb8862d4405c8c5792d988b658682
amoreno <amoreno@15075a66-5c5d-40c6-9c28-692553124c8e>
2018-07-06 08:44:43 +0000

    LOs target no hay que subirlos
    git-svn-id: https://svn.scae.redsara.es/svn/sim/trunk@62 15075a66-5c5d-40c6-9c28-692553124c8e

--- misim-bus/misim-bus-core/target/generated/jaxb/es/minhap/micc/bus/core/generated/package-info.java
+++
@@ -1,9 +0,0 @@
-//
-// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
-// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
-// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2018.06.25 a las 02:30:20 PM CEST 
-//
-
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://intermediacion.redsara.es/scsp/esquemas/V3/soapfaultatributos", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package es.minhap.micc.bus.core.generated;
