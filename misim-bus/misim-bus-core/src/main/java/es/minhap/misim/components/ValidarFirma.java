594411b8bb24c42b52be487945afdd8b439d5dde
MAJORDANA <MAJORDANA@15075a66-5c5d-40c6-9c28-692553124c8e>
2019-02-20 12:10:34 +0000

    git-svn-id: https://svn.scae.redsara.es/svn/sim/trunk@142 15075a66-5c5d-40c6-9c28-692553124c8e

--- misim-bus/misim-bus-core/src/main/java/es/minhap/misim/components/ValidarFirma.java
+++ misim-bus/misim-bus-core/src/main/java/es/minhap/misim/components/ValidarFirma.java
@@ -63,20 +63,10 @@ public class ValidarFirma implements Callable {
 		String tipoFirma= eventContext.getMessage().getOutboundProperty("tipoFirma");
 		
 		try{
-			if(("WS-Security").equals(tipoFirma) || ("EnvioMensajesSSL").equals(tipoFirma)){
-				// Obtenemos el id del servicio de la peticion para poder buscar en la BBDD el
-				// certificado correspondiente, si el tipo de la peticion es de de EnvioMensajesSSL
-				String idServicio = null;
-				if(tipoFirma.equals("EnvioMensajesSSL")){
-					idServicio = descifrar(docFirmado);
-					if(idServicio == null){
-						throw new ModelException("Falta el id de servicio para comprobar la firma", Integer.parseInt(ps.getMessage("plataformaErrores.ValidarFirma.COD_ERROR_GENERAL", null)));
-					}
-				}
-				
-				// Lanzamos la validación de firma
-				if(!firmaService.validarFirmaWSSecurity(docFirmado, idServicio, ps)){
+			if(("ActualizarPassword").equals(tipoFirma) || ("EnvioMensajesWSS").equals(tipoFirma)){
 
+				// Lanzamos la validación de firma y desencriptacion
+				if(!firmaService.validarFirmaWSSecurity(docFirmado, ps)){
 					//Lanzar error
 					LOG.error("Validar Firma: Firma no válida");
 					throw new ModelException("Firma no válida",305);
@@ -102,42 +92,6 @@ public class ValidarFirma implements Callable {
 
 		LOG.debug("Validación de firma realizada.");
 		return eventContext.getMessage();
-	}
-
-	private String descifrar(Document docFirmado) throws Exception {
-		// TODO Auto-generated method stub
-		// Desciframos el documento cifrado
-		Document docDescifrado = cifradoService.descifrarKey(
-				docFirmado,
-				props.getProperty(KeyStoreUtils.KEY_STORE_TYPE),
-				props.getProperty(KeyStoreUtils.KEY_STORE_PASSWORD),
-				props.getProperty(KeyStoreUtils.KEY_STORE_ALIAS),
-				props.getProperty(KeyStoreUtils.ALIAS_PASSWORD),
-				props.getProperty(KeyStoreUtils.KEY_STORE_FILE));
-		
-		docDescifrado = cifradoService.descifrar(
-				docDescifrado,
-				props.getProperty(KeyStoreUtils.KEY_STORE_TYPE),
-				props.getProperty(KeyStoreUtils.KEY_STORE_PASSWORD),
-				props.getProperty(KeyStoreUtils.KEY_STORE_ALIAS),
-				props.getProperty(KeyStoreUtils.ALIAS_PASSWORD),
-				props.getProperty(KeyStoreUtils.KEY_STORE_FILE));
-		
-		NodeList peticion = docDescifrado.getElementsByTagNameNS("http://misim.redsara.es/misim-bus-webapp/peticion", "Peticion");
-		String xmlPeticion = "";
-		if(peticion.item(0)!=null){
-			xmlPeticion = XMLUtils.nodeToString(peticion.item(0));
-			PeticionXMLBean peticionXML = new PeticionXMLBean();
-			peticionXML.loadObjectFromXML(xmlPeticion);
-			String idServicio = peticionXML.getServicio();
-			return idServicio;
-		}
-		else {
-			peticion = docDescifrado.getElementsByTagNameNS("http://misim.redsara.es/misim-bus-webapp/peticion", "ActualizarPasswordPeticion");
-			xmlPeticion = XMLUtils.nodeToString(peticion.item(0));
-			return "ActualizarPassword";
-		}
-		
 	}
 
 }
