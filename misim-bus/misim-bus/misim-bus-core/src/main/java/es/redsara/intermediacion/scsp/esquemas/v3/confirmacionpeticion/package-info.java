331d96571daaef346b85361681e47feadfd2e888
NFERNANDEZ <NFERNANDEZ@15075a66-5c5d-40c6-9c28-692553124c8e>
2019-08-05 12:06:12 +0000

    Eliminacion del doble misim-bus que se encuentra en el SVN, dentro del proyecto principal. sreal20190805
    git-svn-id: https://svn.scae.redsara.es/svn/sim/trunk@258 15075a66-5c5d-40c6-9c28-692553124c8e

--- misim-bus/misim-bus/misim-bus-core/src/main/java/es/redsara/intermediacion/scsp/esquemas/v3/confirmacionpeticion/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://intermediacion.redsara.es/scsp/esquemas/V3/confirmacionPeticion", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package es.redsara.intermediacion.scsp.esquemas.v3.confirmacionpeticion;
--- misim-bus/misim-bus/misim-bus-core/src/main/java/es/redsara/intermediacion/scsp/esquemas/v3/solicitudrespuesta/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://intermediacion.redsara.es/scsp/esquemas/V3/solicitudRespuesta", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package es.redsara.intermediacion.scsp.esquemas.v3.solicitudrespuesta;
