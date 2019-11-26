800a1d20e4fcae2922ed980a4ef1732998591b01
NFERNANDEZ <NFERNANDEZ@15075a66-5c5d-40c6-9c28-692553124c8e>
2019-11-26 17:22:35 +0000

    Octubre2019
    git-svn-id: https://svn.scae.redsara.es/svn/sim/trunk@401 15075a66-5c5d-40c6-9c28-692553124c8e

--- PlataformaMensajeriaIOP/src/es/minhap/plataformamensajeria/iop/dao/impl/QueryExecutorServidoresOrganismosImpl.java
+++ PlataformaMensajeriaIOP/src/es/minhap/plataformamensajeria/iop/dao/impl/QueryExecutorServidoresOrganismosImpl.java
@@ -1,5 +1,9 @@
 package es.minhap.plataformamensajeria.iop.dao.impl;
 
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
@@ -86,5 +90,41 @@ public class QueryExecutorServidoresOrganismosImpl extends HibernateDaoSupport i
 			throw new ApplicationException(e);
 		}
 		return res;
+	}
+
+
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	@Transactional
+	public List<Long> obtenerOrganismosNoActivos(Long idProveedorSMS) {
+		List<Long> res = new ArrayList<Long>();
+
+		try {
+			if (log.isDebugEnabled()) {
+				log.debug(LOG_START);
+			}
+			StringBuilder stringQuery = new StringBuilder();
+			stringQuery.append("SELECT serv.organismoID FROM TBL_SERVIDORES_ORGANISMOS serv");
+			stringQuery.append(" INNER JOIN TBL_ORGANISMOS org ON serv.organismoid = org.organismoid");
+			stringQuery.append(" WHERE serv.SERVIDORID=");
+			stringQuery.append(idProveedorSMS);
+			stringQuery.append(" AND org.activo = 1");
+			SQLQuery query = getSessionFactory().getCurrentSession().createSQLQuery(stringQuery.toString());
+			
+			List<Object> rows = query.list();
+			for (Object row : rows) {
+				res.add(((BigDecimal) row).longValue());
+			}		
+			
+			if (log.isDebugEnabled()) {
+				log.debug(LOG_END);
+			}
+
+		} catch (Exception e) {
+			log.error(HAS_ERROR, e);
+			throw new ApplicationException(e);
+		}
+		return res;	
 	}
 }
